969628a2fdc5aef145e2da1e7325cddb74548494
quentin.moutte.etu <quentin.moutte.etu@univ-lille.fr>
2023-12-10 13:29:22 +0100

    truc

--- javascript/ChangeProjet.js
+++ javascript/ChangeProjet.js
@@ -1,38 +1,27 @@
 function changeProjet(sectionNumber) {
     // Masquer toutes les sections
-    document.querySelectorAll('.projet1, .projet2, .projet3, .projet4, .projet5').forEach(function(section) {
+    document.querySelectorAll('.projet1, .projet2, .projet3').forEach(function(section) {
         section.classList.remove('active');
     });
 
     // Afficher la section sélectionnée
     document.getElementById('projet' + sectionNumber).classList.add('active');
-}
 
-function changeFormation(sectionNumber) {
-    // Masquer toutes les sections
-    document.querySelectorAll('.formation1, .formation2').forEach(function(section) {
+    // Masquer toutes les buttons
+    document.querySelectorAll('.button1, .button2, .button3').forEach(function(section) {
         section.classList.remove('active');
     });
 
     // Afficher la section sélectionnée
-    document.getElementById('formation' + sectionNumber).classList.add('active');
+    document.getElementById('button' + sectionNumber).classList.add('active');
 }
 
-function changeComp(sectionNumber) {
-    // Masquer toutes les sections
-    document.querySelectorAll('.comp1, .comp2, .comp3, .comp4, .comp5, .comp6').forEach(function(section) {
-        section.classList.remove('active');
-    });
+// function changeFormation(sectionNumber) {
+//     // Masquer toutes les sections
+//     document.querySelectorAll('.formation1, .formation2').forEach(function(section) {
+//         section.classList.remove('active');
+//     });
 
-    // Afficher la section sélectionnée
-    document.getElementById('comp' + sectionNumber).classList.add('active');
-}
-
-const nav = document.querySelector("nav");
-
-window.addEventListener("scroll", function () {
-  const offset = window.pageYOffset;
-
-  if (offset > 90) nav.classList.add("scroll");
-  else nav.classList.remove("scroll");
-});
+//     // Afficher la section sélectionnée
+//     document.getElementById('formation' + sectionNumber).classList.add('active');
+// }
