c8bf8a7fda14c58797fab1351c5b75724e686e8a
quentin.moutte.etu <quentin.moutte.etu@univ-lille.fr>
2024-04-15 01:54:11 +0200

    Update new SAE

--- javascript/ChangeProjet.js
+++ javascript/ChangeProjet.js
@@ -1,6 +1,6 @@
 function changeProjet(sectionNumber) {
     // Masquer toutes les sections
-    document.querySelectorAll('.projet1, .projet2, .projet3').forEach(function(section) {
+    document.querySelectorAll('.projet1, .projet2, .projet3, .projet4, .projet5, .projet6').forEach(function(section) {
         section.classList.remove('active');
     });
 
@@ -8,20 +8,10 @@ function changeProjet(sectionNumber) {
     document.getElementById('projet' + sectionNumber).classList.add('active');
 
     // Masquer toutes les buttons
-    document.querySelectorAll('.button1, .button2, .button3').forEach(function(section) {
+    document.querySelectorAll('.button1, .button2, .button3, .button4, .button5, .button6').forEach(function(section) {
         section.classList.remove('active');
     });
 
     // Afficher la section sélectionnée
     document.getElementById('button' + sectionNumber).classList.add('active');
 }
-
-// function changeFormation(sectionNumber) {
-//     // Masquer toutes les sections
-//     document.querySelectorAll('.formation1, .formation2').forEach(function(section) {
-//         section.classList.remove('active');
-//     });
-
-//     // Afficher la section sélectionnée
-//     document.getElementById('formation' + sectionNumber).classList.add('active');
-// }
